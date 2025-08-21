a10bd492369c647a8e8256097c85eb20f7f092a5
aric.zheng <1290657123@qq.com>
2025-08-21 14:34:51 +0800

    feat: new scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
-(function () {
-  'use strict';
+'use strict';
 
-  var gulp = require('gulp');
-  var path = require('path');
-  var fs = require('fs');
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-dart-sass'); // 使用 gulp-dart-sass
+const fs = require('fs');
+const $ = require('gulp-load-plugins')({
+  pattern: ['gulp-*', 'gulp.*', 'del', '@jswork/gulp-*'],
+});
 
-  //import
-  fs.readdirSync('./build').map(function (file) {
-    require('./build/' + file);
-  });
 
-  gulp.task('default', gulp.series(['clean', 'puregrid', 'styles']));
-})();
+//clean
+gulp.task('clean', function() {
+  return $.del(['dist', '.tmp']);
+});
+
+gulp.task('puregrid', function() {
+  return gulp
+    .src([
+      'src/index.scss',
+      'src/grids/_grids-core.scss',
+      'src/grids/_grids-units.scss',
+      'src/grids/_grids-responsive.scss',
+    ])
+    .pipe($.replace('pure-', '#{$wsui-purecss-grid-prefix}'))
+    .pipe($.concat('index.scss'))
+    .pipe(gulp.dest('.tmp'));
+});
+
+gulp.task('styles', function() {
+  return gulp
+    .src('.tmp/index.scss')
+    .pipe($.jswork.pkgHeader())
+    .pipe(sass({ silenceDeprecations: ['legacy-js-api'] }))
+    .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('default', gulp.series(['clean', 'puregrid', 'styles']));
